64cf590bddcd8d5f126a88f954fc109c26299c61
Amir Pourhadi <Alex.CE1379@Gmail.com>
2021-11-16 22:58:35 +0330

    Create session

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const path = require("path");
+const session = require("express-session");
+const { v4: uuidv4 } = require("uuid");
+
+// Run express
 const app = express();
 
+// Define port
 const port = process.env.PORT || 3000;
 
 // JSON Parser
@@ -14,11 +19,15 @@ app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 // Render ejs views
 app.set("view engine", "ejs");
 
+// Session
+app.use(session({ secret: uuidv4(), resave: false, saveUninitialized: true }));
+
 // home route
 app.get("/", (req, res) => {
   res.render("base", { title: "Login System" });
 });
 
+// Listen on port
 app.listen(port, () => {
   console.log(`Server started on http://localhost:${port}`);
 });
