cc8b5b85968e1658ad869bb9f64430c440db40e7
Amir Pourhadi <Alex.CE1379@Gmail.com>
2021-11-20 11:33:25 +0330

    export router

--- router.js
+++ router.js
@@ -10,8 +10,11 @@ const credential = {
 router.post("/login", (req, res) => {
   if (req.body.email === credential.email && req.body.password === credential.password) {
     req.session.user = req.body.email;
-    res.redirect("/dashboard");
+    // res.redirect("/dashboard");
+    res.end("Login Success");
   } else {
     res.send("Invalid Credentials");
   }
 });
+
+module.exports = router;
