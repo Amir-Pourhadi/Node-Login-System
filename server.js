7b52fe9580a96f8997768bd15f64bc2e5ccb7140
Amir Pourhadi <Alex.CE1379@Gmail.com>
2021-11-20 13:22:50 +0330

    Use body parser express

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 const session = require("express-session");
 const { v4: uuidv4 } = require("uuid");
+const router = require("./router");
 
 // Run express
 const app = express();
@@ -11,6 +12,7 @@ const port = process.env.PORT || 3000;
 
 // JSON Parser
 app.use(express.json({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 
 // Serve static assets
 app.use("/static", express.static(path.join(__dirname, "public")));
@@ -23,7 +25,7 @@ app.set("view engine", "ejs");
 app.use(session({ secret: uuidv4(), resave: false, saveUninitialized: true }));
 
 // Routes
-app.use("/route", require("./router"));
+app.use("/route", router);
 
 // home route
 app.get("/", (req, res) => {
